6e4b7536ac04c07242ddd20a74e1b46afc5a5e51
trandangkhoa26 <122193335+trandangkhoa26@users.noreply.github.com>
2023-04-13 13:08:08 +0700

    Hotel Manager JAVA

---
+++ HotelManager/src/main/java/com/mycompany/hotelmanager/HotelManager.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hotelmanager;
+
+/**
+ *
+ * @author Admins
+ */
+public class HotelManager {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
