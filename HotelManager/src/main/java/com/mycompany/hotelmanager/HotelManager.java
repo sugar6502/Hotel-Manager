ba5b7b33e0bd4873dde1ef579aa64541b148d23e
Khoa Trần Đăng <trandgkhoa2602@gmail.com>
2023-06-11 13:06:16 +0700

    Update

--- HotelManager/src/main/java/com/mycompany/hotelmanager/HotelManager.java
+++ HotelManager/src/main/java/com/mycompany/hotelmanager/HotelManager.java
@@ -11,6 +11,6 @@ package com.mycompany.hotelmanager;
 public class HotelManager {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+       new Login().setVisible(true);
     }
 }
