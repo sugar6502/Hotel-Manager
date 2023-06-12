89c639b06c85f1c889a062c27889ae5745f7f7c6
Khoa Trần Đăng <trandgkhoa2602@gmail.com>
2023-06-12 12:12:37 +0700

    Update

--- HotelManager/src/main/java/com/mycompany/hotelmanager/Source_code.java
+++ HotelManager/src/main/java/com/mycompany/hotelmanager/Source_code.java
@@ -20,20 +20,21 @@ public class Source_code {
             PreparedStatement pres = con.prepareStatement(sql);
             rs = pres.executeQuery();   
         }
-        catch(Exception e) {JOptionPane.showMessageDialog(null, "Lỗi");}
+        catch(Exception e) {JOptionPane.showMessageDialog(null, "Lỗi hàm LayLoaiPhong");}
         return rs;
     }
     
     public int LayMaLoaiPhong(String tenLP){
-    String sql = "select MaLoaiPhong from LOAIPHONG where TenLoaiPhong = N'?'";
-    int ketqua=0;
+    String sql = "select MaLoaiPhong from LOAIPHONG where TenLoaiPhong = N'"+ tenLP + "' ";
+    int ketqua = 0;
     try{
         PreparedStatement pres = con.prepareStatement(sql);
-        pres.setString(1,tenLP);       
+        //pres.setString(1,tenLP);       
         ResultSet rs = pres.executeQuery();   
+        rs.next();
         ketqua = rs.getInt(1);
         }
-    catch(Exception e) {JOptionPane.showMessageDialog(null, "Lỗi");}
+    catch(Exception e) {JOptionPane.showMessageDialog(null, "Lỗi hàm LayMaLoaiPhong");}
     return ketqua;
     }
     
@@ -46,7 +47,28 @@ public class Source_code {
             pres.setString(2,Integer.toString(tinhtrang));  
             rs = pres.executeQuery();   
         }
-        catch(Exception e) {JOptionPane.showMessageDialog(null, "Lỗi");}
+        catch(Exception e) {JOptionPane.showMessageDialog(null, "Lỗi hàm LayPhong");}
+        return rs;
+    }
+    
+    public void XetTinhTrangPhong(int sophong, int tinhtrang){
+        String sql = "update PHONG" + " Set TinhTrang = "+Integer.toString(tinhtrang) + " Where SoPhong = "+ Integer.toString(sophong);
+        ResultSet rs = null;
+        try{
+            PreparedStatement pres = con.prepareStatement(sql);
+            pres.executeUpdate();   
+        }
+        catch(Exception e) {JOptionPane.showMessageDialog(null, "Lỗi hàm XetTinhTrangPhong");}
+    }
+    
+    public ResultSet PhongTinhTrang2(){
+    String sql = "select LOAIPHONG.TenLoaiPhong, PHONG.SoPhong, PHONG.GhiChu from PHONG, LOAIPHONG where PHONG.MaLoaiPhong = LOAIPHONG.MaLoaiPhong and PHONG.TinhTrang = 2";
+    ResultSet rs = null;
+        try{
+            PreparedStatement pres = con.prepareStatement(sql);
+            rs = pres.executeQuery();   
+        }
+        catch(Exception e) {JOptionPane.showMessageDialog(null, "Lỗi hàm PhongTinhTrang2");}
         return rs;
     }
 }
